d343a6c1c4435e49d8712ea2c9c10b93fa889570
Milena Carvalho <nascarvalho.m@gmail.com>
2023-07-02 15:56:11 -0300

    study typeorm

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
